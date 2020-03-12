dd580885e1d4285e65789288b488573bca32c198
Wlodek Dymaczewski <dymaczew@Wlodeks-MacBook-Pro.local>
2020-03-12 11:01:25 +0100

    Added smartcam

---
+++ smartcam/movedetection/build/v1/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+docker login --username=acmegrocery --password=5f47ecdd-a6cb-4a08-b99a-c03286e61954
+
+# Build and execute a simple NodeJS application that will respond to a CURL request to PORT 2020
+echo *INFO* finding and removing any previous instances
+id=`docker ps -a|grep -i battery_amd64|awk '{print $1}'`
+docker stop ${id}
+docker rm ${id}
+docker rmi acmegrocery/battery_amd64:v1
+echo *INFO* building a new NodeJS container image
+docker build -t acmegrocery/battery_amd64:v1 .
+sudo docker push acmegrocery/battery_amd64:v1
+
---
+++ smartcam/movedetection/build/v1/publish.sh
@@ -0,0 +1,1 @@
+hzn exchange service publish -O -I -f service.definition.json -p service.policy.json -v
